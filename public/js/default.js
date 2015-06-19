f5f453a3da0bde0f9a8f1dd42e29c4834c32cea0
Sven Ullmann <sven.ullmann@e3n.de>
2015-06-20 01:03:53 +0200

    fixing error with fit height

--- public/js/default.js
+++ public/js/default.js
@@ -3,15 +3,56 @@
 	$(document).ready(function(){
 		
 		var fitrow = function(){
+			
+			var windowWidth = $(window).width();			
+			if(windowWidth<768){
+				var lookfor = 'xs';
+			} else if(windowWidth<992){
+				var lookfor = 'sm';
+			} else if(windowWidth<1200){
+				var lookfor = 'md';
+			} else {
+				var lookfor = 'lg';
+			}
+				
 			$('.row-fit-height').each(function(){
 				var row = $(this);
 				var height = 0;
 				$(this).children().each(function(){
 					if($(this).height() > height){
-						height = $(this).height();
+						switch(lookfor){
+							case 'lg':
+								var has = 
+									$(this).hasClass('col-lg-full-height')
+									|| $(this).hasClass('col-md-full-height')
+									|| $(this).hasClass('col-sm-full-height')
+									|| $(this).hasClass('col-sx-full-height')
+								break;
+							case 'md':
+								var has = 
+									$(this).hasClass('col-md-full-height')
+									|| $(this).hasClass('col-sm-full-height')
+									|| $(this).hasClass('col-sx-full-height')
+								break;
+							case 'sm':
+								var has = 
+									$(this).hasClass('col-sm-full-height')
+									|| $(this).hasClass('col-sx-full-height')
+								break;
+							case 'xs':
+								var has = $(this).hasClass('col-sx-full-height')
+								break;
+						}
+						if(has){
+							height = $(this).height();
+						}
 					}
 				});
-				row.css({height:height});				
+				if(0==height){
+					row.css({height:'auto'});
+				} else {
+					row.css({height:height});
+				}
 			});
 		} 
 		
